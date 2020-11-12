014204c0a90001775ab9a835794442b231b511fe
Mylyn-1122 <26zhengmylyn@gmail.com>
2020-11-11 18:54:33 -0800

    test

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 var START = 0;
 var PLAY = 1;
 var END = 2;
-var backgroundImg, bubbleImg, swimmingBoyImg, spikeImg, startImg, restartImg;
+var backgroundImg, bubbleImg, swimmingBoyImg, spikeImg, startImg, restartImg, startBackgroundImg;
 var swimmingBoy, bubble, obstacle, start;
 var score = 0;
 var air = 20;
 var obstacleGroup
 var gameState = START
 var bubbleGroup;
-var obstaclenumber = 200
+
 
 
 function preload() {
@@ -19,6 +19,7 @@ function preload() {
   spike2Img = loadImage("Images/Spike2.png");
   startImg = loadImage("Images/Start.png");
   restartImg = loadImage("Images/Restart.png");
+  startBackgroundImg = loadImage("Images/underwater.png");
 }
 
 function setup() {
@@ -26,7 +27,7 @@ function setup() {
   Background = createSprite(400, 200, 800, 400);
   Background.addImage(backgroundImg);
   Background.scale = 2.3;
-  Background.velocityX = -3;
+  Background.velocityX = 0;
   swimmingBoy = createSprite(100, 200, 30, 30);
   swimmingBoy.addAnimation("image", swimmingBoyImg);
   swimmingBoy.scale = 0.5;
@@ -38,7 +39,7 @@ function setup() {
 
   
 
-  swimmingBoy.debug = true;
+  //swimmingBoy.debug = true;
 
   obstacleGroup = new Group();
   bubbleGroup = new Group();
@@ -53,13 +54,18 @@ function draw() {
   background("black"); 
   
   if(gameState === START){
-    Background.visible = false;
+    Background.visible = true;
+    Background.addImage(startBackgroundImg);
+    Background.scale = 3.7
     swimmingBoy.visible = false;
     start.visible = true;
     restart.visible = false
 
     if(mousePressedOver(start)){
-      gameState = PLAY
+      gameState = PLAY;
+      Background.addImage(backgroundImg);
+      Background.velocityX = -3;
+      Background.scale = 2.3;
     }
 
     drawSprites()
@@ -74,26 +80,26 @@ function draw() {
       Background.x = 700;
     }
 
-    if(frameCount % 1600 === 0){
-      obstaclenumber -= 5
-    }
+    
+  ;
+    
 
     start.visible = false;
     Background.visible = true;
     swimmingBoy.visible = true;
     restart.visible = false
 
-    if(keyDown("down")){
+    if(keyDown("down")&&swimmingBoy.y<400){
       swimmingBoy.y += 20
     }
-    if(keyDown("up")){
+    if(keyDown("up")&&swimmingBoy.y>0){
       swimmingBoy.y -= 20
     }
 
     score = score + 0.08
     air = air - 0.02
 
-    if(frameCount % obstaclenumber === 0){
+    if(frameCount % 200 === 0){
       var number = Math.round(random(1,2))
       if(number === 1){
         spawnObstacle()
@@ -105,9 +111,11 @@ function draw() {
 
     spawnBubble()
 
-    if(bubbleGroup.isTouching(swimmingBoy)){
-      air += 7;
-      bubbleGroup.destroyEach()
+    for(var i=0; i < bubbleGroup.length; i++){
+      if(bubbleGroup.get(i).isTouching(swimmingBoy)){
+        air += 4;
+        bubbleGroup.get(i).destroy();
+      }
     }
 
     drawSprites()
@@ -139,14 +147,19 @@ function draw() {
 
   if(gameState === END){
     swimmingBoy.visible = false
-    Background.visible = false
+    Background.visible = true
     restart.visible = true
     start.visible = false
 
+    Background.addImage(startBackgroundImg);
+    Background.scale = 3.7;
+    Background.velocityX = 0
+    Background.x = 400
+
     drawSprites();
 
-    textSize(20);
-    fill("white")
+    textSize(30);
+    fill("black")
     text("score : " + Math.round(score), 400, 100)
 
     if(mousePressedOver(restart)){
@@ -166,9 +179,9 @@ function spawnObstacle(){
     obstacle = createSprite(800, 75, 30, 30);
     obstacle.addImage(spikeImg);
     obstacle.scale = random(1, 1.7);
-    obstacle.velocityX = -3;
+    obstacle.velocityX = -(3+score/100);
     obstacle.lifetime = 240;
-    obstacle.debug = true;
+    //obstacle.debug = true;
     obstacle.setCollider("rectangle", 0, 0, 50, 120)
     obstacleGroup.add(obstacle)
   
@@ -179,20 +192,20 @@ function spawnObstacleD(){
     obstacle = createSprite(800, 325, 30, 30);
     obstacle.addImage(spike2Img);
     obstacle.scale = random(1, 1.7);
-    obstacle.velocityX = -3;
+    obstacle.velocityX = -(3+score/100);
     obstacle.lifetime = 240;
-    obstacle.debug = true;
+    //obstacle.debug = true;
     obstacle.setCollider("rectangle", 0, 0, 50, 120)
     obstacleGroup.add(obstacle)
   
 }
 
 function spawnBubble(){
-  if(frameCount % 250 === 0){
+  if(frameCount % 150 === 0){
     bubble = createSprite(800, random(30, 270), 20, 20);
     bubble.addImage(bubbleImg);
     bubble.scale = 0.10;
-    bubble.velocityX = -4;
+    bubble.velocityX = -(3+score/100);
     bubble.lifetime + 210;
     bubbleGroup.add(bubble)
   }
@@ -201,7 +214,6 @@ function spawnBubble(){
 function restartFunction(){
   obstacleGroup.destroyEach();
   bubbleGroup.destroyEach();
-  obstaclenumber = 200;
   air = 20;
   score = 0;
   gameState = START;
